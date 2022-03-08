d129607beb93aad276a02772bd66470e0222e070
Tyler Swett <tyler.swett@me.com>
2022-03-08 12:41:13 -0600

    Making it more 3d

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 const MAX_X_STRETCH = 255;
 const MAX_Y_STRETCH = 255;
+const MAX_X_ROTATION = 30;
+const MAX_Y_ROTATION = 30;
 
-let tyler = document.getElementById("tyler");
+let largeNameItems = document.querySelectorAll(".large-name");
+let nameContainer = document.getElementById("name-container");
 
 let windowHeight = window.innerHeight;
 let windowWidth = window.innerWidth;
@@ -14,7 +17,10 @@ function updateHeightWidth() {
 }
 
 document.addEventListener('mousemove', updateShadow)
+document.addEventListener('touchstart', updateShadow);
+document.addEventListener('touchend', removeShadow)
 
+let shadowX, shadowY;
 function updateShadow(e) {
     let screenXCenter = window.innerWidth/2;
     let screenYCenter = window.innerHeight/2;
@@ -33,7 +39,32 @@ function updateShadow(e) {
     }
 
 
-    tyler.style.textShadow = generateShadow(percentFromCenterX*MAX_X_STRETCH,percentFromCenterY*MAX_Y_STRETCH);
+    shadowX = percentFromCenterX * MAX_X_STRETCH;
+    shadowY = percentFromCenterY * MAX_Y_STRETCH;
+    rotateY = percentFromCenterX * MAX_Y_ROTATION * -1;
+    rotateX = percentFromCenterY * MAX_X_ROTATION;
+    largeNameItems[0].style.textShadow = generateShadow(shadowX,shadowY);
+    largeNameItems[1].style.textShadow = generateShadow(shadowX,shadowY);
+    if(shadowY>0) {
+        largeNameItems[1].style.zIndex = 100;
+        largeNameItems[0].style.zIndex = 1;
+    } else {
+        largeNameItems[0].style.zIndex = 100;
+        largeNameItems[1].style.zIndex = 1;
+    }
+    nameContainer.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) perspective(200px)`;
+}
+
+function touchStart(e) {
+
+}
+
+function touchMove(e) {
+
+}
+
+function removeShadow(e) {
+
 }
 
 function generateShadow(x,y) {
